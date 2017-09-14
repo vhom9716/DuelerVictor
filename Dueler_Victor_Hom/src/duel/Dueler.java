14023aea67bc69ce04a7bde36c47a705ede639c0
BT_1N3_01 <BT_1N3_01@BT_1N3_01.bktech.local>
2017-09-14 10:56:17 -0400

    dueler

--- Dueler_Victor_Hom/src/duel/Dueler.java
+++ Dueler_Victor_Hom/src/duel/Dueler.java
@@ -10,5 +10,10 @@ public interface Dueler {
 	//returns the name of the Dueler
 	String getName();
 	
+	//set the value of a field. Write this in such a way that it can only be called once (by the Duel, at the start)
+	void setStartingHP(int hp);
+	//returns the value of HP, can be called as often as anyone wants
+	int getHP();
+	
 	
 }
