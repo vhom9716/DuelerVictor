e876399dc8d215bb40c09200b5c565202bfcdd99
BT_1N3_01 <BT_1N3_01@BT_1N3_01.bktech.local>
2017-09-14 11:43:46 -0400

    added code

--- Dueler_Victor_Hom/src/duel/CharacterB.java
+++ Dueler_Victor_Hom/src/duel/CharacterB.java
@@ -2,20 +2,24 @@ package duel;
 
 public class CharacterB implements Dueler {
 
+	private int hp;
+	public CharacterB() {
+		
+	}
 	public void taunt() {
-
+		System.out.println("You're terrible!");
 	}
 
 	public String getName() {
-
+		return "Joker";
 	}
 	
-	public void getStartingHP() {
-		
+	public void getStartingHP(int hp) {
+		this.hp = hp;
 	}
 	
-	public int getHP(int hp) {
-		
+	public int getHP() {
+		return hp;
 	}
 	
 	public boolean determineIfOpponentIsFair(Dueler d, int hp) {
