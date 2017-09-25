f405dc276690a710e6ada3723666fe440396de1e
BT_1N3_01 <BT_1N3_01@BT_1N3_01.bktech.local>
2017-09-25 11:25:37 -0400

    get action updated

--- Dueler_Victor_Hom/src/duel/CharacterB.java
+++ Dueler_Victor_Hom/src/duel/CharacterB.java
@@ -3,9 +3,13 @@ package duel;
 public class CharacterB implements Dueler {
 
 	private int hp;
+	private boolean isLoaded;
 	private String[] taunts = {"Seeing you makes me want to barf!", "You're terrible!", "Fight me!"}; 
 	
-	public CharacterB() {}
+	public CharacterB() {
+		isLoaded = false;
+		
+	}
 	public void taunt() {
 		System.out.println(taunts[(int) Math.floor(Math.random()* taunts.length)]);
 	}
@@ -30,10 +34,47 @@ public class CharacterB implements Dueler {
 	}
 	
 	public int getAction(Object caller) {
-		if (caller instanceof Duel) {
-			return (int) Math.floor(Math.random() * 3);
-			} else
-			return 3;
+		if (caller instanceof Duel) {	
+			
+			if(isLoaded) {
+				//1 or 2, not 0
+				if(Math.random() < 0.5) {
+					isLoaded = false;
+					return 1;
+				}
+				else
+					return 2;
+			}else {
+				//0 or 2, not 1
+				if(Math.random() < 0.5) {
+					isLoaded = true;
+					return 0;
+				}
+				else
+					return 2;
+			}
+			
+//			int actionNumber = (int) Math.floor(Math.random() * 3);
+//			if(isLoaded && actionNumber == 1)
+//			{
+//				isLoaded = false;
+//				return 1;
+//			}
+//			while(!isLoaded)
+//			{	if(actionNumber == 0)
+//				{
+//					isLoaded = true;
+//					return 0;
+//				}
+//			
+//				if(actionNumber == 1)
+//					actionNumber = (int) Math.floor(Math.random() * 3);
+//				else
+//					return 2;
+//			}
+//			return 0;
+		} else
+		return 3;
 	}
 	
 	public void hit(Object caller) {
